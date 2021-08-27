44a4a22acc7184fd1d21fe80d304f8cf23db0f39
Dave Shanley <dshanley@vmware.com>
2021-08-27 12:58:01 -0400

    Updated joke service to work correctly.
    Needed to be bootable and to make the correct API call.
    
    Signed-off-by: Dave Shanley <dshanley@vmware.com>

--- plank/services/joke-service.go
+++ plank/services/joke-service.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"github.com/google/uuid"
 	"github.com/vmware/transport-go/model"
 	"github.com/vmware/transport-go/service"
+	"net/http"
 	"reflect"
 )
 
@@ -20,6 +22,11 @@ type Joke struct {
 // JokeService will return a terrible joke, on demand.
 type JokeService struct {
 	core service.FabricServiceCore
+}
+
+// NewJokeService will return an instance of JokeService.
+func NewJokeService() *JokeService {
+	return &JokeService{}
 }
 
 // Init will fire when the service is being registered by the fabric, it passes a reference of the same core
@@ -50,16 +57,52 @@ func (js *JokeService) getJoke(request *model.Request, core service.FabricServic
 
 	// make API call using inbuilt RestService to make network calls.
 	core.RestServiceRequest(&service.RestServiceRequest{
-		Uri:          "https://icanhazdadjoke.com",
-		Method:       "GET",
+		Uri:    "https://icanhazdadjoke.com",
+		Method: "GET",
+		Headers: map[string]string{
+			"Accept": "application/json",
+		},
 		ResponseType: reflect.TypeOf(&Joke{}),
 	}, func(response *model.Response) {
 
 		// send back a successful joke.
-		core.SendResponse(request, []string{response.Payload.(*Joke).Joke})
+		core.SendResponse(request, response.Payload.(*Joke))
 	}, func(response *model.Response) {
 
 		// something went wrong with the API call, tell the requester.
-		core.SendErrorResponse(request, response.ErrorCode, response.ErrorMessage)
+		fabricError := service.GetFabricError("Get Joke API Call Failed", response.ErrorCode, response.ErrorMessage)
+		core.SendErrorResponseWithPayload(request, response.ErrorCode, response.ErrorMessage, fabricError)
 	})
+}
+
+// GetRESTBridgeConfig returns a config for a REST endpoint for Jokes
+func (js *JokeService) GetRESTBridgeConfig() []*service.RESTBridgeConfig {
+	return []*service.RESTBridgeConfig{
+		{
+			ServiceChannel: JokeServiceChannel,
+			Uri:            "/rest/joke",
+			Method:         http.MethodGet,
+			AllowHead:      true,
+			AllowOptions:   true,
+			FabricRequestBuilder: func(w http.ResponseWriter, r *http.Request) model.Request {
+				return model.Request{
+					Id:                &uuid.UUID{},
+					Request:           "get-joke",
+					BrokerDestination: nil,
+				}
+			},
+		},
+	}
+}
+
+// OnServerShutdown is not implemented in this service.
+func (js *JokeService) OnServerShutdown() {}
+
+// OnServiceReady has no functionality in this service, so it returns a fired channel.
+func (js *JokeService) OnServiceReady() chan bool {
+
+	// ready right away, nothing to do.
+	readyChan := make(chan bool, 1)
+	readyChan <- true
+	return readyChan
 }
