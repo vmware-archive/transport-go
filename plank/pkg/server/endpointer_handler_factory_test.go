9b775e94ad8de1c81781a05766e62d070cb098c5
Josh Kim <kjosh@vmware.com>
2021-11-01 10:34:40 -0700

    fix broken unit tests
    Signed-off-by: Josh Kim <kjosh@vmware.com>

--- plank/pkg/server/endpointer_handler_factory_test.go
+++ plank/pkg/server/endpointer_handler_factory_test.go
@@ -16,46 +16,49 @@ import (
 func TestBuildEndpointHandler_Timeout(t *testing.T) {
 	b := bus.ResetBus()
 	service.ResetServiceRegistry()
+	msgChan := make(chan *model.Message, 1)
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 	assert.HTTPBodyContains(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
 		return model.Request{
 			Payload: nil,
 			Request: "test-request",
 		}
-	}, 5*time.Millisecond), "GET", "http://localhost", nil, "request timed out")
+	}, 5*time.Millisecond, msgChan), "GET", "http://localhost", nil, "request timed out")
 }
 
 func TestBuildEndpointHandler_ChanResponseErr(t *testing.T) {
 	b := bus.ResetBus()
 	service.ResetServiceRegistry()
+	msgChan := make(chan *model.Message, 1)
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 	assert.HTTPErrorf(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
 		uId := &uuid.UUID{}
-		_ = b.SendErrorMessage("test-chan", fmt.Errorf("test error"), uId)
+		msgChan <- &model.Message{Error: fmt.Errorf("test error")}
 		return model.Request{
 			Id:      uId,
 			Payload: nil,
 			Request: "test-request",
 		}
-	}, 5*time.Second), "GET", "http://localhost", nil, "test error")
+	}, 5*time.Second, msgChan), "GET", "http://localhost", nil, "test error")
 }
 
 func TestBuildEndpointHandler_SuccessResponse(t *testing.T) {
 	b := bus.ResetBus()
 	service.ResetServiceRegistry()
+	msgChan := make(chan *model.Message, 1)
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 	assert.HTTPBodyContains(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
 		uId := &uuid.UUID{}
-		_ = b.SendResponseMessage("test-chan", &model.Response{
+		msgChan <- &model.Message{Payload: &model.Response{
 			Id:      uId,
 			Payload: []byte("{\"error\": false}"),
-		}, uId)
+		}}
 		return model.Request{
 			Id:      uId,
 			Payload: nil,
 			Request: "test-request",
 		}
-	}, 5*time.Second), "GET", "http://localhost", nil, "{\"error\": false}")
+	}, 5*time.Second, msgChan), "GET", "http://localhost", nil, "{\"error\": false}")
 }
 
 func TestBuildEndpointHandler_ErrorResponse(t *testing.T) {
@@ -63,6 +66,7 @@ func TestBuildEndpointHandler_ErrorResponse(t *testing.T) {
 	service.ResetServiceRegistry()
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 
+	msgChan := make(chan *model.Message, 1)
 	uId := &uuid.UUID{}
 	rsp := &model.Response{
 		Id:        uId,
@@ -73,19 +77,20 @@ func TestBuildEndpointHandler_ErrorResponse(t *testing.T) {
 	expected, _ := json.Marshal(rsp.Payload)
 
 	assert.HTTPBodyContains(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
-		_ = b.SendResponseMessage("test-chan", rsp, uId)
+		msgChan <- &model.Message{Payload: rsp}
 		return model.Request{
 			Id:      uId,
 			Payload: nil,
 			Request: "test-request",
 		}
 
-	}, 5*time.Second), "GET", "http://localhost", nil, string(expected))
+	}, 5*time.Second, msgChan), "GET", "http://localhost", nil, string(expected))
 }
 
 func TestBuildEndpointHandler_ErrorResponseAlternative(t *testing.T) {
 	b := bus.ResetBus()
 	service.ResetServiceRegistry()
+	msgChan := make(chan *model.Message, 1)
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 
 	uId := &uuid.UUID{}
@@ -96,19 +101,20 @@ func TestBuildEndpointHandler_ErrorResponseAlternative(t *testing.T) {
 	}
 
 	assert.HTTPBodyContains(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
-		_ = b.SendResponseMessage("test-chan", rsp, uId)
+		msgChan <- &model.Message{Payload: rsp}
 		return model.Request{
 			Id:      uId,
 			Payload: nil,
 			Request: "test-request",
 		}
 
-	}, 5*time.Second), "GET", "http://localhost", nil, "418")
+	}, 5*time.Second, msgChan), "GET", "http://localhost", nil, "418")
 }
 
 func TestBuildEndpointHandler_CatchPanic(t *testing.T) {
 	b := bus.ResetBus()
 	service.ResetServiceRegistry()
+	msgChan := make(chan *model.Message, 1)
 	_ = b.GetChannelManager().CreateChannel("test-chan")
 	assert.HTTPBodyContains(t, buildEndpointHandler("test-chan", func(w http.ResponseWriter, r *http.Request) model.Request {
 		panic("peekaboo")
@@ -116,5 +122,5 @@ func TestBuildEndpointHandler_CatchPanic(t *testing.T) {
 			Payload: nil,
 			Request: "test-request",
 		}
-	}, 5*time.Second), "GET", "http://localhost", nil, "Internal Server Error")
+	}, 5*time.Second, msgChan), "GET", "http://localhost", nil, "Internal Server Error")
 }
